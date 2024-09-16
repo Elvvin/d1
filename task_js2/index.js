25d716703392f5a02255b3663335e8b875c54ca8
Elvvin <elvin.babazade.1010.eb.eb@gmail.com>
2024-09-16 19:57:47 +0400

    Task 3 Final

--- task_js2/index.js
+++ task_js2/index.js
@@ -34,6 +34,7 @@
 //     return true;
 // }
 
+
 // console.log(arraysAreEqual([1, 2, 3], [1, 2, 3])); // true
 // console.log(arraysAreEqual([1, 2, 3], [3, 2, 1])); // true
 // console.log(arraysAreEqual([1, 2], [1, 2, 2]));   // false
